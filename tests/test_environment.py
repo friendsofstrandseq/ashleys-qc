1c09fb04c0a4eb2ff74c634b824ff178660b7257
ChristinaEimer <s8cheime@stud.uni-saarland.de>
2021-02-10 09:15:51 +0100

    check for warning

--- tests/test_environment.py
+++ tests/test_environment.py
@@ -1,18 +1,18 @@
 import pickle
 import warnings
 import pandas as pd
+import pytest
 
 from ashleyslib.prediction import predict_model
-warnings_as_errors=all
 
 
-def test_pickle_warning(capsys):
+def test_pickle_warning():
     model_name = 'models/svc_default.pkl'
     features = pd.read_csv('data/test_features.tsv', sep='\t')
     features = features.drop(columns=['sample_name'])
 
-    predict_model(model_name, features)
-    captured = capsys.readouterr()
-    assert not captured.err
+    with pytest.warns(None) as record:
+        predict_model(model_name, features)
+    assert len(record) == 0
 
     return True
